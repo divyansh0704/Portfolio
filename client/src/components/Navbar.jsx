096cf4c8458b6a3f0c5019b148cf519b913358c7
divyansh0704 <divyansh298.be22@chitkara.edu.in>
2025-07-15 21:06:12 +0530

    menubar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,15 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
-    <nav>
-      <Link to="/" >Home</Link>
-      <Link to="/projects">Projects</Link>
-      <Link to="/services">Services</Link>
-      <Link to="/contact">Contact</Link>
-      <Link to="/admin/add-project">Admin</Link>
-    </nav>
+    <div className="navbar">
+      <div
+        className="menu-toggle"
+        onClick={() => setMenuOpen((prev) => !prev)}
+      >
+        ☰
+      </div>
+      <nav className={`nav-links ${menuOpen ? 'open' : ''}`}>
+        <Link to="/" >Home</Link>
+        <Link to="/projects">Projects</Link>
+        <Link to="/services">Services</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/admin/add-project">Admin</Link>
+      </nav>
+
+    </div>
   )
 }
 

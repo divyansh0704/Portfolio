c5839a9648be3615dce6f935935ea30345b0e16d
divyansh0704 <divyansh298.be22@chitkara.edu.in>
2025-08-08 14:32:49 +0530

    spelling mistake

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <section className='hero'>
       <h1>Hello! I'm Divyansh</h1>
-      <p>MERN Stack Developer | Full Stack Project Creater</p>
+      <p>MERN Stack Developer | Full Stack Project Creator</p>
     </section>
   )
 }

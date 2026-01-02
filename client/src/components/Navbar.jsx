a5ef106f89b2a3596e393f5d8f06c68f0651d3ad
divyansh0704 <divyansh298.be22@chitkara.edu.in>
2026-01-02 20:11:37 +0530

    little ui changes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,18 +5,19 @@ const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
   return (
     <div className="navbar">
-      <div
+      {/* <div
         className="menu-toggle"
         onClick={() => setMenuOpen((prev) => !prev)}
       >
         ☰
-      </div>
-      <nav className={`nav-links ${menuOpen ? 'open' : ''}`}>
-        <Link to="/" >Home</Link>
+      </div> */}
+      {/* <nav className={`nav-links ${menuOpen ? 'open' : ''}`}> */}
+        <nav className='nav-links'>
+        <Link className='k' to="/" >Home</Link>
         <Link to="/projects">Projects</Link>
         <Link to="/services">Services</Link>
         <Link to="/contact">Contact</Link>
-        <Link to="/admin/add-project">Admin</Link>
+        <Link className='k' to="/admin/add-project">Admin</Link>
       </nav>
 
     </div>
